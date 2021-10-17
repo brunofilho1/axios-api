7c412bdd0235f3496369b3a072b574e3efbcb211
Jakeliny Gracielly <jakeliny.dev@gmail.com>
2021-10-17 02:57:03 -0300

    improve catch

--- main.js
+++ main.js
@@ -1,62 +1,56 @@
 const url = "http://localhost:5500/api"
 
-function getUsers(){
+function getUsers() {
   axios.get(url)
-  .then(response => {
-    const data = response.data
+    .then(response => {
+      const data = response.data
 
-    renderApiResult.textContent = JSON.stringify(data)
-  })
-  .catch(error => console.log(error))
+      renderApiResult.textContent = JSON.stringify(data)
+    })
+    .catch(error => console.log(error))
 }
 
-function getUser(){
+function getUser() {
   axios.get(`${url}/1`)
-  .then(response => {
-    const data = response.data
+    .then(response => {
+      const data = response.data
 
       userAvatar.src = data.avatar
       userNome.textContent = data.name
       userId.textContent = data.id
       userCity.textContent = data.city
-  })
-  .catch(error => console.log(error))
+    })
+    .catch(error => console.log(error))
 }
 
-function addNewUser(newUser){
+function addNewUser(newUser) {
 
   axios.post(url, newUser)
-  .then(response => {
-    alert( JSON.stringify(response.data))
-    getUsers()
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+    .then(response => {
+      alert(JSON.stringify(response.data))
+      getUsers()
+    })
+    .catch(error => console.error(error));
 }
 
-function updateUser(user, id){
+function updateUser(user, id) {
   axios.put(`${url}/${id}`, user)
-  .then(response => {
-    alert( JSON.stringify(response.data))
-    getUsers()
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+    .then(response => {
+      alert(JSON.stringify(response.data))
+      getUsers()
+    })
+    .catch(error => console.error(error));
 }
 
-function deleteUser(id){
+function deleteUser(id) {
   axios.delete(`${url}/${id}`)
-  .then(response => {
-    alert( JSON.stringify(response.data))
-    getUsers()
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+    .then(response => {
+      alert(JSON.stringify(response.data))
+      getUsers()
+    })
+    .catch(error => console.error(error));
 }
- 
+
 //calls
 getUsers()
 getUser()
