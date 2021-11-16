99fa816cd562d35e386084bcc1b00bdf5015253b
Bruno Oliveira <brunowalker551@gmail.com>
2021-11-16 16:26:51 -0300

    added get and post methods

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
-const url='http://localhsot:5500/api';
+const url='http://localhost:5500/api';
+
+const newUser = {
+  name: 'Brunera',
+  avatar: 'https://github.com/brunofilho1.png',
+  city: 'Brasilândia de Minas'
+};
 
 function getUser() {
-  axios.get(url)
-  .then(res => {
-    console.log(res);
-  })
-  .catch(error => console.log(error))
+  axios.get(url).then(res => {
+    const data = res.data;
+    renderApiResult.textContent = JSON.stringify(data);
+  }).catch(error => console.log(error))
 };
 
 getUser();
+
+function addNewUser() {
+  axios.post(url, newUser).then(res => {
+    console.log(res.data);
+    alert(JSON.stringify(res.data));
+  }).catch(error => console.log(error));
+}
+
+addNewUser();
