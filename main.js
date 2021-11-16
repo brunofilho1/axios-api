f9932dce2084a57aba870d02289260845e4bdb56
Bruno Oliveira <brunowalker551@gmail.com>
2021-11-16 16:42:30 -0300

    added update and delete methods

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
+
 const url='http://localhost:5500/api';
 
 const newUser = {
   name: 'Brunera',
   avatar: 'https://github.com/brunofilho1.png',
   city: 'Brasilândia de Minas'
+};
+const updatedUser = {
+  name: 'CABEÇA DE PREGO',
+  avatar: 'https://github.com/brunofilho1.png',
+  city: 'São Paulo'
 };
 
 function getUser() {
@@ -19,7 +25,23 @@ function addNewUser() {
   axios.post(url, newUser).then(res => {
     console.log(res.data);
     alert(JSON.stringify(res.data));
+    document.location.reload(true);
   }).catch(error => console.log(error));
 }
+/* addNewUser(); */
 
-addNewUser();
+function updateUser() {
+  axios.put(`${url}/5`, updatedUser).then(res => {
+    console.log(res.data);
+    alert(JSON.stringify(res.data));
+    document.location.reload(true);
+  }).catch(error => console.log(error))
+}
+/* updateUser() */
+
+function deleteUser() {
+  axios.delete(`${url}/7`).then(res => {
+    alert(JSON.stringify(res.data));
+    document.location.reload(true);
+  }).catch(error => console.log(error))
+}
